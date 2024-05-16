cf38f33b8ef9dee64f43b908e2c952a41979a25d
ariaandika <andikaakun17@gmail.com>
2024-05-17 00:08:36 +0700

    add: home and login

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,40 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+import Blob from "$/lib/ui/Blob.svelte";
+    import { button_ghost, button_primary } from "$/lib/ui/button";
+// import { UserData, logout, roleToUrl } from "#/users"
+// import { arrowRight } from "$/ui/icon";
+
+// const user = UserData();
+
+</script>
+
+<header class="[ p-6 top-0 left-0 w-full fixed flex justify-between ] font-bold">
+    <div class="w-full flex gap-8 justify-center">
+        <div>Documentation</div>
+        <div>Help</div>
+        <div>Call Center</div>
+    </div>
+    <div class="p-6 top-0 right-0 fixed ">
+        Login
+    </div>
+</header>
+
+<!-- Page -->
+<section class="px-4 w-full h-full grid place-items-center">
+    <Blob/>
+    <div class="text-center ">
+        <h1 class="text-6xl font-bold tracking-tight">BANTER Staff Web App</h1>
+        <p class="mt-6 text-gray-600 text-lg">
+            Welcome to BANTER, if you are a staff, please login to continue
+        </p>
+        <div class="my-6 w-full flex gap-4 justify-center">
+            <a href="/sales" class="{button_primary}">
+                Get Started
+            </a>
+            <a href="/login" class="{button_ghost}">
+                Login
+            </a>
+        </div>
+    </div>
+</section>
+
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,57 @@
+<script>
+import Blob from "$/lib/ui/Blob.svelte"
+import { input } from "$/lib/ui/input";
+import { page } from "$app/stores"
+import { onMount } from "svelte";
+
+/** @type {HTMLInputElement} */
+let inputName;
+
+onMount(() => {
+    inputName.focus();
+})
+
+</script>
+
+<a class="fixed left-8 top-8 hover:bg-gray-400/40 p-3 rounded-full" href="/">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+        class="w-6 h-6"
+    >
+        <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
+    </svg>
+</a>
+
+<section class="h-full grid place-items-center">
+    <Blob/>
+    <form class="w-[380px]" on:submit|preventDefault={()=>{}}>
+        <h1 class="text-4xl font-bold text-center text-indigo-600">BANTER</h1>
+        <h1 class="text-2xl font-bold text-center">Login to your account</h1>
+
+        <div class="my-8"></div>
+
+        <label class="flex flex-col">
+            <span aria-errormessage="phone">Phone</span>
+            <input type="text" name="phone" autocomplete="tel" required class="{input}" bind:this={inputName}>
+        </label>
+
+        <div class="my-4"></div>
+
+        <label class="flex flex-col">
+            <span>Password</span>
+            <input type="password" name="password" autocomplete="current-password" required class={input}>
+        </label>
+
+        <div class="my-4"></div>
+
+        <button
+        class="px-3 py-2 w-full rounded-md
+               font-bold text-white text-center text-base
+               bg-indigo-600 hover:bg-indigo-600/90"
+        >Login</button>
+
+        <div class="text-sm text-red-600">{$page.form?.error?.message ?? ""}</div>
+    </form>
+
+</section>
+
+
