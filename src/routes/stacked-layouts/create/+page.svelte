16759ffe6184b433725e7bccd0d97c83c302ab4b
ariaandika <andikaakun17@gmail.com>
2024-05-17 16:24:46 +0700

    update: create order page submission

--- src/routes/stacked-layouts/create/+page.svelte
+++ src/routes/stacked-layouts/create/+page.svelte
@@ -1,30 +1,74 @@
 <script>
-import { button_primary } from "$/lib/ui/button";
-import { input } from "$/lib/ui/input"
+import { CreateOrderStore } from "./data"
 import { Plus, X } from "../icon";
+import { onMount } from "svelte"
+import { button_primary, button_ghost } from "$/lib/ui/button";
+import { input } from "$/lib/ui/input"
+import { cn } from "$/lib"
 
-let selectedPackage = 1;
+let selectedPackage = 0;
+let data = CreateOrderStore();
+
+function add() {
+    data.addPackage();
+    selectedPackage += 1;
+}
+
+function remove() {
+    if ($data.packages.length <= 1) {
+        return;
+    }
+    let target = selectedPackage;
+    selectedPackage = selectedPackage <= 0 ? selectedPackage : selectedPackage - 1;
+    data.removePackage(target);
+}
+
+/** @param {SubmitEvent} e */
+function submit(e) {
+    e.preventDefault();
+    data.submit()
+}
+
+function clear() {
+    data.clear()
+}
+
+/** @param {FocusEvent} e */
+function focusOut(e) {
+    const target = /** @type {HTMLButtonElement} */(e.target);
+    if (target.id == "clear") {
+        return;
+    }
+    data.snapshot();
+}
+
+onMount(() => {
+    return () => data.snapshot();
+})
 
 </script>
 
-
+<svelte:window on:beforeunload={() => data.snapshot()}></svelte:window>
 
 <section class="flex gap-4 flex-col">
     <h1 class="text-white text-4xl font-bold">Create Orders</h1>
 
-    <form method="post" class="p-8 bg-white rounded-md shadow-md">
+    <form method="post" class="relative p-8 bg-white rounded-md shadow-md" on:submit={submit} on:focusout={focusOut}>
+        <div class="absolute mx-8 right-0">
+            <button id="clear" class="{cn(button_ghost, 'hover:bg-red-600 hover:text-white transition-colors')}" type="button" on:click={clear} tabindex="-1">Clear All</button>
+        </div>
 
         <h2 class="py-1 text-2xl font-bold border-b border-gray-200">Sender</h2>
 
         <section class="flex gap-4 justify-between">
             <label class="mt-2 flex flex-col flex-1">
                 <span class="text-sm text-gray-800">Name</span>
-                <input type="text" class="{input}" required>
+                <input type="text" class="{input}" required bind:value={$data.sender.name}>
             </label>
 
             <label class="mt-2 flex flex-col flex-1">
                 <span class="text-sm text-gray-800">Phone</span>
-                <input type="text" class="{input}" required>
+                <input type="text" class="{input}" required bind:value={$data.sender.phone}>
             </label>
         </section>
 
@@ -34,12 +78,12 @@ let selectedPackage = 1;
         <section class="mt-2 flex gap-4 justify-between">
             <label class="flex flex-col flex-1">
                 <span class="text-sm text-gray-800">Name</span>
-                <input type="text" class="{input}" required>
+                <input type="text" class="{input}" required bind:value={$data.receiver.name}>
             </label>
 
             <label class="flex flex-col flex-1">
                 <span class="text-sm text-gray-800">Phone</span>
-                <input type="text" class="{input}" required>
+                <input type="text" class="{input}" required bind:value={$data.receiver.phone}>
             </label>
         </section>
 
@@ -48,37 +92,37 @@ let selectedPackage = 1;
 
         <label class="mt-2 flex flex-col flex-1">
             <span class="text-sm text-gray-800">Detail Address</span>
-            <input type="text" class="{input}" required>
+            <input type="text" class="{input}" required bind:value={$data.destination.detail}>
         </label>
 
         <section class="mt-2 flex gap-4 justify-between">
             <label class="flex flex-col flex-1">
                 <span class="text-sm text-gray-800">Kelurahan</span>
-                <input type="text" class="{input}" required>
+                <input type="text" class="{input}" required bind:value={$data.destination.kelurahan}>
             </label>
 
             <label class="flex flex-col flex-1">
                 <span class="text-sm text-gray-800">Kecamatan</span>
-                <input type="text" class="{input}" required>
+                <input type="text" class="{input}" required bind:value={$data.destination.kecamatan}>
             </label>
         </section>
 
         <section class="mt-2 flex gap-4 justify-between">
             <label class="mt-2 flex flex-col flex-1">
                 <span class="text-sm text-gray-800">Kabupaten</span>
-                <input type="text" class="{input}" required>
+                <input type="text" class="{input}" required bind:value={$data.destination.kabupaten}>
             </label>
 
             <label class="mt-2 flex flex-col flex-1">
                 <span class="text-sm text-gray-800">Provinsi</span>
-                <input type="text" class="{input}" required>
+                <input type="text" class="{input}" required bind:value={$data.destination.provinsi}>
             </label>
         </section>
 
         <section class="mt-2 flex gap-4 justify-between">
             <label class="mt-2 flex flex-col flex-1">
                 <span class="text-sm text-gray-800">Zipcode</span>
-                <input type="number" class="{input}" required>
+                <input type="number" class="{input}" required bind:value={$data.destination.kodepos}>
             </label>
             <div class="flex-1">&ThickSpace;</div>
         </section>
@@ -88,42 +132,47 @@ let selectedPackage = 1;
 
         <label class="mt-2 flex flex-col flex-1">
             <span class="text-sm text-gray-800">Name</span>
-            <input type="text" class="{input}" required>
+            <input type="text" class="{input}" required bind:value={$data.packages[selectedPackage].name}>
         </label>
 
         <section class="mt-2 flex gap-4 justify-between">
             <label class="flex flex-col flex-1">
                 <span class="text-sm text-gray-800">Weight</span>
-                <input type="number" class="{input}" required>
+                <input type="number" class="{input}" required bind:value={$data.packages[selectedPackage].weight}>
             </label>
 
             <label class="flex flex-col flex-1">
                 <span class="text-sm text-gray-800">Length</span>
-                <input type="number" class="{input}" required>
+                <input type="number" class="{input}" required bind:value={$data.packages[selectedPackage].length}>
             </label>
 
             <label class="flex flex-col flex-1">
                 <span class="text-sm text-gray-800">Width</span>
-                <input type="number" class="{input}" required>
+                <input type="number" class="{input}" required bind:value={$data.packages[selectedPackage].width}>
             </label>
 
             <label class="flex flex-col flex-1">
                 <span class="text-sm text-gray-800">Height</span>
-                <input type="number" class="{input}" required>
+                <input type="number" class="{input}" required bind:value={$data.packages[selectedPackage].height}>
             </label>
         </section>
 
-        <section class="mt-4 flex gap-4">
-            Selected Package: <div class="flex">
-                {#each {length:4} as _,i}
-                    <button class="px-3 py-0.5 {selectedPackage == i ? 'bg-gray-200' : 'hover:bg-gray-100'} text-center border-y border-r border-gray-400 first:border-l first:rounded-l-md" type="button" on:click={() => selectedPackage = i}>{i + 1}</button>
-                {/each}
-                <button class="px-1 py-0.5 hover:bg-gray-100 text-gray-600 text-center border-y border-r border-gray-400 rounded-r-md *:h-3/4" type="button">
-                    {@html Plus}
-                </button>
-                <button class="mx-2 px-1 py-0.5 hover:bg-red-600 text-gray-600 hover:text-white text-center border border-gray-400 transition-colors rounded-md *:h-3/4" type="button">
-                    {@html X}
-                </button>
+        <section class="mt-4 flex justify-between">
+            <div class="flex gap-4 align-center">
+                <div>Selected Package: </div>
+                <div>
+                    <div class="flex">
+                        {#each { length: $data.packages.length } as _,i}
+                            <button class="px-3 py-0.5 {selectedPackage == i ? 'bg-gray-200' : 'hover:bg-gray-100'} text-center border-y border-r border-gray-400 first:border-l first:rounded-l-md" type="button" on:click={() => selectedPackage = i}>{i + 1}</button>
+                        {/each}
+                        <button class="px-1 py-0.5 hover:bg-gray-100 text-gray-600 text-center border-y border-r border-gray-400 rounded-r-md *:h-3/4" type="button" on:click={add}>
+                            {@html Plus}
+                        </button>
+                        <button class="mx-2 px-1 py-0.5 hover:bg-red-600 text-gray-600 hover:text-white text-center border border-gray-400 transition-colors rounded-md *:h-3/4" type="button" on:click={remove}>
+                            {@html X}
+                        </button>
+                    </div>
+                </div>
             </div>
         </section>
 
--- src/routes/stacked-layouts/create/data.ts
+++ src/routes/stacked-layouts/create/data.ts
@@ -1,34 +1,95 @@
-import { Array, Literal, Number, Object, String, Union } from "@sinclair/typebox"
+import { applyAction } from "$app/forms";
+import { page } from "$app/stores";
+import { Array, Literal, Number as DefNumber, Object, String, Union, Null } from "@sinclair/typebox"
+import { Errors } from "@sinclair/typebox/errors";
+import { Check, Create } from "@sinclair/typebox/value";
+import { get, writable } from "svelte/store"
+
+const BASE = "/sales";
+const URL = "http://localhost:3000/sales/v1/orders";
+const CREATE_ORDER_CTX = 'CREATE_ORDER_CTX';
+
+export const CreateOrderStore = () => {
+    const snapshot = sessionStorage.getItem(CREATE_ORDER_CTX);
+    const store = writable<typeof CreateOrderRequest.static>(snapshot ? JSON.parse(snapshot) : Create(CreateOrderRequest))
+
+    function clear() {
+        sessionStorage.removeItem(CREATE_ORDER_CTX);
+        store.set(Create(CreateOrderRequest))
+    }
+
+    return {
+        ...store, clear,
+        addPackage() {
+            store.update(e => {
+                e.packages.push(Create(PackageData));
+                return e;
+            })
+        },
+        removePackage(i: number) {
+            store.update(e => {
+                e.packages.splice(i, 1);
+                return e;
+            })
+        },
+        snapshot() {
+            sessionStorage.setItem(CREATE_ORDER_CTX, JSON.stringify(get(store)));
+        },
+        async submit() {
+            const input = get(store);
+            console.log({Mutation: input})
+            if (!Check(CreateOrderRequest, input)) {
+                const err = Errors(CreateOrderRequest, input).First()!;
+                console.error({SubmissionError: err})
+                return await applyAction({ type: "failure", status: 422, data: { message: err.message + ": " + err.path.replaceAll("/",".") } });
+            }
+            try {
+                if (get(page).status == 10) {
+                    return console.warn("Submission is in progress");
+                }
+
+                const response = await fetch(URL,{
+                    method: "POST",
+                    mode: "cors",
+                    credentials: "include",
+                    body: JSON.stringify(input)
+                });
+                const data = await response.json();
+
+                if (response.status >= 400) {
+                    return await applyAction({ type: "failure", status: response.status, data })
+                }
+
+                await applyAction({ type: "success", status: response.status, data: { message: "Create Order Success" } })
+                clear();
+                await applyAction({ type: "redirect", status: 302, location: BASE });
+            } catch (error) {
+                await applyAction({ type: "error", error })
+            }
+        }
+    }
+}
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const Number = () => DefNumber({ default: null });
 
 const Snapshot = Union([
     Object({
-        type: Literal("id"),
-        user_id: Number(),
-    }),
-    Object({
-        type: Literal("anon"),
+        type: Union([Literal("id"),Literal("anon")],{ default: "anon" }),
+        user_id: Union([Number(),Null()]),
         name: String(),
         phone: String(),
     })
 ])
+
+const PackageData = Object({
+    name: String(),
+    weight: Number(),
+    length: Number(),
+    width: Number(),
+    height: Number(),
+})
 
 export const CreateOrderRequest = Object({
     sender: Snapshot,
@@ -41,12 +102,6 @@ export const CreateOrderRequest = Object({
         provinsi: String(),
         kodepos: Number(),
     }),
-    packages: Array(Object({
-        name: String(),
-        weight: Number(),
-        length: Number(),
-        width: Number(),
-        height: Number(),
-    }))
+    packages: Array(PackageData, { default: [Create(PackageData)] })
 })
 
